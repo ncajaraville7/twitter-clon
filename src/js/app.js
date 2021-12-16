78fbbe65c176c2634a57581d478462ca65e48262
Nicolas Cajaraville <nicolas.cajaraville7@gmail.com>
2021-12-16 12:41:31 -0300

    Agregado Borrar Tweets

--- src/js/app.js
+++ src/js/app.js
@@ -67,9 +67,9 @@ function crearTweet() {
 
     listaTweets.forEach( tweet => {
         const tweetDiv = document.createElement('div');
+        tweetDiv.classList.add('tweets')
 
         tweetDiv.innerHTML = `
-        <div class="tweets">
                 <div class="tweets__contenido">
                     <div class="tweets__contenido__imagen">
                         <img src="src/imagenes/imagenPerfil.png" alt="Imagen de Perfil">
@@ -98,14 +98,25 @@ function crearTweet() {
                     <div class="tweets__botones__boton">
                         <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" focusable="false" class="tweetIcon-compartir" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M400 317.7h73.9V656c0 4.4 3.6 8 8 8h60c4.4 0 8-3.6 8-8V317.7H624c6.7 0 10.4-7.7 6.3-12.9L518.3 163a8 8 0 0 0-12.6 0l-112 141.7c-4.1 5.3-.4 13 6.3 13zM878 626h-60c-4.4 0-8 3.6-8 8v154H214V634c0-4.4-3.6-8-8-8h-60c-4.4 0-8 3.6-8 8v198c0 17.7 14.3 32 32 32h684c17.7 0 32-14.3 32-32V634c0-4.4-3.6-8-8-8z"></path></svg>
                     </div>
-                </div>
-            </div>
+                </div> 
         `;
 
+        const btnBorrar = document.createElement('a')
+        btnBorrar.textContent = '';
+        btnBorrar.classList.add('btnBorrar');
+        btnBorrar.onclick = () => {
+            borrarTweet(tweet.id)
+        }
+
+        tweetDiv.appendChild(btnBorrar);
         tweetsLista.appendChild(tweetDiv);
     });
-
     guardarTweet();
+}
+
+function borrarTweet(id) {
+    listaTweets = listaTweets.filter( tweet => tweet.id !== id);
+    crearTweet();
 }
 
 function guardarTweet() {
